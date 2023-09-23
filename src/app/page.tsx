80f7c4f3497fe87f1bab45cc4e6b2a9e3743fabe
nigesh-flywire <nigesh.shakya@flywire.com>
2023-09-23 18:10:16 -0400

    Auth with OPT working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,24 @@ import { LeftSideBar } from '@/components/LeftSideBar';
 import { Tweet } from '../components/Tweet';
 import { MainComponent } from '../components/MainComponent';
 import { RightSideBar } from '@/components/RightSideBar';
-import { Dialog, DialogContent } from '@radix-ui/react-dialog';
-
 import { cookies } from 'next/headers'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import type { Database } from '@/lib/database.types'
-import { Input } from '@/components/ui/input';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from '@/components/ui/button';
+import { ED } from '@/components/ED';
+import { Toaster } from 'sonner';
 
 export const dynamic = 'force-dynamic'
 
@@ -17,26 +29,22 @@ export const Home = async () => {
 
   const { data, error } = await supabase.auth.getUser()
 
-  console.log({ data, error })
-
-
+  // console.log({ data, error })
 
   return (
+    <>
+      <Toaster position='top-center' />
+      <ED></ED>
 
-    <div className='w-full h-full flex relative justify-center items-center text-black'>
-      {/* <Dialog defaultOpen={error?.status === 401}>
-        <DialogContent>
-          <Input></Input>
-        </DialogContent>
-      </Dialog> */}
-
-      <div className='max-w-screen-xl w-full h-full flex relative'>
-        {/* Left side bard for navigation or header */}
-        <LeftSideBar />
-        <MainComponent />
-        <RightSideBar />
+      <div className='w-full h-full flex relative justify-center items-center text-black'>
+        <div className='max-w-screen-xl w-full h-full flex relative'>
+          {/* Left side bard for navigation or header */}
+          <LeftSideBar />
+          <MainComponent />
+          <RightSideBar />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 export default Home
---
+++ src/components/ED.tsx
@@ -0,0 +1,72 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import { Dialog, DialogContent, DialogFooter, DialogHeader } from './ui/dialog';
+import { Input } from './ui/input';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { Label } from '@radix-ui/react-label';
+import { Button } from './ui/button';
+import { toast } from 'sonner'
+
+
+
+export const ED = () => {
+    const redirectUrl = 'http://localhost:3001'
+    const supabase = createClientComponentClient()
+    const [isOpen, setIsOpen] = useState(false);
+    const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        supabase.auth.getSession().then((res) => {
+            console.log({ res })
+            if (!res.data.session) {
+                setIsOpen(true);
+            }
+        })
+    })
+
+    return (
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+            <DialogContent className='border-none rounded-full'>
+                <form onSubmit={async (e) => {
+                    e.preventDefault()
+
+                    setIsLoading(true)
+
+                    const { data, error } = await supabase.from("profiles").select().eq("username", username.trim())
+
+                    if (data && data.length > 0) {
+                        setIsLoading(false)
+                        return toast.error('Username already found')
+                    }
+
+                    if (error) {
+                        setIsLoading(false)
+                        return toast.error(error.message)
+                    }
+
+                    await supabase.auth.signInWithOtp({
+                        email: email.trim(),
+                        options: {
+                            emailRedirectTo: redirectUrl,
+                            data: {
+                                username: username.trim()
+                            }
+                        }
+                    })
+
+                    setIsLoading(false)
+
+                }}>
+                    <div className='flex flex-col space-y-4 text-white'>
+                        <Label className='text-3xl font-bold py-4'>Sign in to X</Label>
+                        <Input type="email" placeholder='Email' className='border-opacity-80 border-gray-500/50 h-14' onChange={e => setEmail(e.target.value)}></Input>
+                        <Input type="text" placeholder='Username' className='border-opacity-80 border-gray-500/50 h-14' onChange={e => setUsername(e.target.value)}></Input>
+                        <Button disabled={isLoading} className='bg-white w-full text-black rounded-2xl'>Next</Button>
+                    </div>
+                </form>
+            </DialogContent>
+        </Dialog>
+    )
+}
